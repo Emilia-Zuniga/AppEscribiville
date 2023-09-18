f567c080786e3b6a03f59d69f4cd0d9932a19578
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-09-18 16:54:40 -0600

    Initial Commit

---
+++ AppEscribiville/AppEscribivilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEscribivilleApp.swift
+//  AppEscribiville
+//
+//  Created by Emilia Zuñiga Losada on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppEscribivilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
